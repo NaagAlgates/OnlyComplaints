47d2c4a7290323ca8579013f9a84ec7c5d5443b9
Nagaraj Alagusundaram <me@nagaraj.com.au>
2025-08-14 11:30:49 +1000

    Added publish parameter

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
   },
   server: {
     open: true
