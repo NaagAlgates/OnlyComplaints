b79d59c9d77e79ae6c6596aeec01c68020507b6a
Nagaraj Alagusundaram <me@nagaraj.com.au>
2025-08-14 11:15:53 +1000

    Added Publish parameters

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
   server: {
     open: true
